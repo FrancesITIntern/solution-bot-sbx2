b90a4e5b59b816358cb2e3d946e7a2da0c9bf2db
FrancesITIntern <Frances.abadilla@frieslandcampina.com>
2025-06-25 11:51:46 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,9 @@
-python -m chainlit run app.py --host 0.0.0.0 --port 8000
+##python -m chainlit run app.py --host 0.0.0.0 --port 8000
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+PORT=${PORT:-8000}
+                                                               
+python -m chainlit run app.py --host 0.0.0.0 --port $PORT
+

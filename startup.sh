88d2534f0acbf1bb934dfcc082eb475176049a57
FrancesITIntern <Frances.abadilla@frieslandcampina.com>
2025-06-26 17:18:10 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,2 @@
-#!/bin/bash
 
-# Upgrade pip first
-python -m pip install --upgrade pip
-
-# Install dependencies
-python -m pip install -r requirements.txt
-
-# Set port variable (default 8000)
-PORT=${PORT:-8000}
-
-# Run the Chainlit app
-python -m chainlit run app.py --host 0.0.0.0 --port $PORT
+python -m chainlit run app.py --host 0.0.0.0 --port 8000
